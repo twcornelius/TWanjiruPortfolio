a46541cce0a10a0963f1e98f221c794cbb971dd3
twcornelius <twcornelius13@gmail.com>
2022-03-28 20:38:45 -0400

    59

--- index.js
+++ index.js
@@ -20,18 +20,7 @@ document.getElementById("defaultOpen").click();
 
 
 
-function myFunction(imgs) {
-  // Get the expanded image
-  var expandImg = document.getElementById("expandedImg");
-  // Get the image text
-  var imgText = document.getElementById("imgtext");
-  // Use the same src in the expanded image as the image being clicked on from the grid
-  expandImg.src = imgs.src;
-  // Use the value of the alt attribute of the clickable image as text inside the expanded image
-  imgText.innerHTML = imgs.alt;
-  // Show the container element (hidden with CSS)
-  expandImg.parentElement.style.display = "block";
-}
+
 
 
 
@@ -56,8 +45,17 @@ $('nav').hover(function(){
   
   });
     
-  $(function() {
-    $('#close').click(function() {
-        $('#float_bottom_left').remove();
+  $(function () {
+    "use strict";
+    
+    $(".popup img").click(function () {
+        var $src = $(this).attr("src");
+        $(".show").fadeIn();
+        $(".img-show img").attr("src", $src);
     });
+    
+    $("span, .overlay").click(function () {
+        $(".show").fadeOut();
+    });
+    
 });
