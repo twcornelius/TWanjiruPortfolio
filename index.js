8afaf8daba361bc517b5de1a16198a47d35d0b7d
twcornelius <twcornelius13@gmail.com>
2022-03-29 19:33:53 -0400

    61

--- index.js
+++ index.js
@@ -45,17 +45,4 @@ $('nav').hover(function(){
   
   });
     
-  $(function () {
-    "use strict";
-    
-    $(".popup img").click(function () {
-        var $src = $(this).attr("src");
-        $(".show").fadeIn();
-        $(".img-show img").attr("src", $src);
-    });
-    
-    $("span, .overlay").click(function () {
-        $(".show").fadeOut();
-    });
-    
-});
+   
